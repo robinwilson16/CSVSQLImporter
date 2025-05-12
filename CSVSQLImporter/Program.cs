8069e7da6c57697f2e4da4786fd47cd383ddb1fa
Robin Wilson <robin.wilson@robindigital.co.uk>
2025-05-12 07:52:15 +0100

    Added log file that is generated in same folder as .exe file each time the process runs

--- CSVSQLImporter/Program.cs
+++ CSVSQLImporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using CSVSQLImporter.Services;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualBasic;
@@ -16,12 +17,19 @@ namespace CSVSQLImporter
     {
         static async Task<int> Main(string[] args)
         {
-            Console.WriteLine($"\nImport CSV File to SQL Table");
-            Console.WriteLine($"=========================================\n");
+            bool? logToFile = true;
+            bool? outputToScreen = true;
+
+            string? toolName = Assembly.GetExecutingAssembly().GetName().Name;
+            string logFileName = $"{toolName} - {DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss")}.log";
+
+            await LoggingService.Log(toolName, logFileName, logToFile, outputToScreen);
+            await LoggingService.Log($"Import CSV File to SQL Table", logFileName, logToFile, outputToScreen);
+            await LoggingService.Log($"=========================================", logFileName, logToFile, outputToScreen);
 
             string? productVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
-            Console.WriteLine($"Version {productVersion}");
-            Console.WriteLine($"Copyright Robin Wilson");
+            await LoggingService.Log($"\nVersion {productVersion}", logFileName, logToFile, outputToScreen);
+            await LoggingService.Log($"\nCopyright Robin Wilson", logFileName, logToFile, outputToScreen);
 
             string configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
             string? customConfigFile = null;
@@ -35,7 +43,7 @@ namespace CSVSQLImporter
                 configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, customConfigFile);
             }
 
-            Console.WriteLine($"\nUsing Config File {configFile}");
+            await LoggingService.Log($"\nUsing Config File {configFile}", logFileName, logToFile, outputToScreen);
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -48,11 +56,11 @@ namespace CSVSQLImporter
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error: {0}", e);
+                await LoggingService.Log($"Error: {e}", logFileName, logToFile, outputToScreen);
                 return 1;
             }
 
-            Console.WriteLine($"\nSetting Locale To {config["Locale"]}");
+            await LoggingService.Log($"\nSetting Locale To {config["Locale"]}", logFileName, logToFile, outputToScreen);
 
             //Set locale to ensure dates and currency are correct
             CultureInfo culture = new CultureInfo(config["Locale"] ?? "en-GB");
@@ -146,8 +154,8 @@ namespace CSVSQLImporter
                         break;
                 }
 
-                Console.WriteLine("\nDownloading CSV File");
-                Console.WriteLine($"Downloding File {csvFile["FileName"]} From {sessionOptions.HostName}");
+                await LoggingService.Log("\nDownloading CSV File", logFileName, logToFile, outputToScreen);
+                await LoggingService.Log($"Downloding File {csvFile["FileName"]} From {sessionOptions.HostName}", logFileName, logToFile, outputToScreen);
 
                 try
                 {
@@ -180,24 +188,24 @@ namespace CSVSQLImporter
                         // Print results
                         foreach (TransferEventArgs transfer in transferResult.Transfers)
                         {
-                            Console.WriteLine("Download of {0} succeeded from {1}", transfer.FileName, downloadpath);
+                            await LoggingService.Log($"Download of {transfer.FileName} succeeded from {downloadpath}", logFileName, logToFile, outputToScreen);
                         }
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Error: {0}", e);
+                    await LoggingService.Log($"Error: {e}", logFileName, logToFile, outputToScreen);
                     return 1;
                 }
             }
             else
             {
-                Console.WriteLine($"Not Downloading File to FTP as Option in Config is False");
+                await LoggingService.Log($"Not Downloading File to FTP as Option in Config is False", logFileName, logToFile, outputToScreen);
             }
 
             //Load CSV File
-            Console.WriteLine("\nLoading Data from CSV");
-            Console.WriteLine($"Loading CSV File from {csvFilePath}");
+            await LoggingService.Log("\nLoading Data from CSV", logFileName, logToFile, outputToScreen);
+            await LoggingService.Log($"Loading CSV File from {csvFilePath}", logFileName, logToFile, outputToScreen);
 
             DataTable table;
 
@@ -235,7 +243,7 @@ namespace CSVSQLImporter
                     }
                 }
 
-                Console.WriteLine($"Loaded {csvData.Count} rows of data from {csvFilePath}");
+                await LoggingService.Log($"Loaded {csvData.Count} rows of data from {csvFilePath}", logFileName, logToFile, outputToScreen);
 
                 //Testing outputs
                 //int rowIndexTest = 0;
@@ -245,7 +253,7 @@ namespace CSVSQLImporter
 
                 //    //fieldValues = fileRow?.Split(csvFile["Delimiter"]) ?? Array.Empty<string>();
 
-                //    Console.WriteLine($"Row {rowIndexTest} - {fileRow[2]}");
+                //    await LoggingService.Log($"Row {rowIndexTest} - {fileRow[2]}");
                 //}
 
                 int rowIndex = 0;
@@ -314,7 +322,7 @@ namespace CSVSQLImporter
                                     //Output types to check for specific column
                                     //if (colIndex == 35)
                                     //{
-                                    //    Console.WriteLine($"Value '{fieldRowValue}' is {fieldType}");
+                                    //    await LoggingService.Log($"Value '{fieldRowValue}' is {fieldType}");
                                     //}
                                 }
 
@@ -400,7 +408,7 @@ namespace CSVSQLImporter
                                 }
                                 
 
-                                //Console.WriteLine($"\n{fieldHeaderValue}");
+                                //await LoggingService.Log($"\n{fieldHeaderValue}");
 
                                 //Add field to the table
                                 DataColumn tableColumn = new DataColumn(colName, Type.GetType(fieldTypeToUse) ?? typeof(string));
@@ -445,19 +453,19 @@ namespace CSVSQLImporter
                 table?.AcceptChanges();
 
                 //Testing outputs
-                //Console.WriteLine($"\nValue {table?.Rows[0][1]} {table?.Rows[0][1].GetType()}");
+                //await LoggingService.Log($"\nValue {table?.Rows[0][1]} {table?.Rows[0][1].GetType()}");
             }
             else
             {
-                Console.WriteLine($"The File at {csvFilePath} Could Not Be Found");
+                await LoggingService.Log($"The File at {csvFilePath} Could Not Be Found", logFileName, logToFile, outputToScreen);
                 return 1;
             }
 
-            Console.WriteLine($"Loaded {table?.Rows.Count} rows of data from file");
+            await LoggingService.Log($"Loaded {table?.Rows.Count} rows of data from file", logFileName, logToFile, outputToScreen);
 
             //Save to Database
-            Console.WriteLine("\nSaving Data To Database");
-            Console.WriteLine($"Creating Table {table?.TableName} in Database");
+            await LoggingService.Log("\nSaving Data To Database", logFileName, logToFile, outputToScreen);
+            await LoggingService.Log($"Creating Table {table?.TableName} in Database", logFileName, logToFile, outputToScreen);
             await using var connection = new SqlConnection(connectionString);
             try
             {
@@ -466,7 +474,7 @@ namespace CSVSQLImporter
                 if (table != null)
                 {
                     string? createTableSQL = CreateTableSQL(schemaName ?? "dbo", table?.TableName ?? "Imported_CSV_File", table!);
-                    //Console.WriteLine($"{createTableSQL}");
+                    //await LoggingService.Log($"{createTableSQL}");
 
                     using (SqlCommand command = new SqlCommand(createTableSQL, connection))
                         await command.ExecuteNonQueryAsync();
@@ -474,7 +482,7 @@ namespace CSVSQLImporter
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                await LoggingService.Log(e.ToString(), logFileName, logToFile, outputToScreen);
 
                 if (connection != null)
                 {
@@ -484,7 +492,7 @@ namespace CSVSQLImporter
                 return 1;
             }
 
-            Console.WriteLine($"Uploading {table?.Rows.Count} Rows of Data into Table {table?.TableName} in Database");
+            await LoggingService.Log($"Uploading {table?.Rows.Count} Rows of Data into Table {table?.TableName} in Database", logFileName, logToFile, outputToScreen);
 
             try
             {
@@ -495,7 +503,7 @@ namespace CSVSQLImporter
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                await LoggingService.Log(e.ToString(), logFileName, logToFile, outputToScreen);
 
                 if (connection != null)
                 {
@@ -508,8 +516,8 @@ namespace CSVSQLImporter
             //Run Stored Procedure On Completion
             if (storedProcedure.GetValue<bool?>("RunTask", false) == true)
             {
-                Console.WriteLine("\nRunning Post Import Stored Procedure");
-                Console.WriteLine($"Running Stored Procedure: {storedProcedure["Database"]}.{storedProcedure["Schema"]}.{storedProcedure["StoredProcedure"]}");
+                await LoggingService.Log("\nRunning Post Import Stored Procedure", logFileName, logToFile, outputToScreen);
+                await LoggingService.Log($"Running Stored Procedure: {storedProcedure["Database"]}.{storedProcedure["Schema"]}.{storedProcedure["StoredProcedure"]}", logFileName, logToFile, outputToScreen);
 
                 if (storedProcedure["StoredProcedure"]?.Length > 0)
                 {
@@ -518,17 +526,17 @@ namespace CSVSQLImporter
                         if (table != null)
                         {
                             string customTaskSQL = $"EXEC {storedProcedure["Database"]}.{storedProcedure["Schema"]}.{storedProcedure["StoredProcedure"]}";
-                            //Console.WriteLine($"{createTableSQL}");
+                            //await LoggingService.Log($"{createTableSQL}");
 
                             using (SqlCommand command = new SqlCommand(customTaskSQL, connection))
                                 await command.ExecuteNonQueryAsync();
 
-                            Console.WriteLine($"Stored Procedure Completed");
+                            await LoggingService.Log($"Stored Procedure Completed", logFileName, logToFile, outputToScreen);
                         }
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine(e.ToString());
+                        await LoggingService.Log(e.ToString(), logFileName, logToFile, outputToScreen);
 
                         if (connection != null)
                         {
@@ -540,7 +548,7 @@ namespace CSVSQLImporter
                 }
                 else
                 {
-                    Console.WriteLine($"Cannot run stored procedure as it has not been specified in the config file");
+                    await LoggingService.Log($"Cannot run stored procedure as it has not been specified in the config file", logFileName, logToFile, outputToScreen);
                 }
             }
 
@@ -560,7 +568,7 @@ namespace CSVSQLImporter
             sqlsc += $"\n CREATE TABLE [{schemaName ?? "dbo"}].[{tableName}] (";
 
             //Check Cell Value
-            //Console.WriteLine(table.Rows[1][4].ToString());
+            //await LoggingService.Log(table.Rows[1][4].ToString());
 
             for (int i = 0; i < table.Columns.Count; i++)
             {
@@ -605,7 +613,7 @@ namespace CSVSQLImporter
                         for (int rowIndex = 0; rowIndex < table.Rows.Count; rowIndex++)
                         {
                             rowLength = (table.Rows[rowIndex][i].ToString() ?? "").Length;
-                            //Console.WriteLine($"Column: {i}, Row: {rowIndex}, Length: {rowLength}");
+                            //await LoggingService.Log($"Column: {i}, Row: {rowIndex}, Length: {rowLength}");
 
                             if (rowLength > maxRowLength)
                             {
